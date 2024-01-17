457f945b0253b1422b06747f5bb7be1b3efef387
Nsarakenidis <nikoreve@hotmail.gr>
2024-01-17 17:39:39 +0200

    Add authenticated user dto, Fix bug when trying to find a championship existence

---
+++ backend/spring-server/src/main/java/io/github/mixaniki/controller/dto/AuthResponseDto.java
@@ -0,0 +1,4 @@
+package io.github.mixaniki.controller.dto;
+
+public record AuthResponseDto(String token, String username, String userDetails) {
+}
--- backend/spring-server/src/main/java/io/github/mixaniki/domain/model/service/ParticipationService.java
+++ backend/spring-server/src/main/java/io/github/mixaniki/domain/model/service/ParticipationService.java
@@ -42,9 +42,10 @@ public interface ParticipationService extends ObjectService<Participation, Parti
      * @throws NotFoundException In case team or championship do not exist or participation already exists
      */
     @Validated(value = {ValidationGroups.Update.class, Default.class} )
-    Participation update(@Valid @NotNull Team team, @Valid @NotNull Championship championship,
+    Participation update(@Valid @NotNull Team team,
+                         @Valid @NotNull Championship championship,
                          @Valid @NotNull Participation participation) throws NotFoundException;
 
-    boolean existsDrawById(@Valid @NotNull Championship championship);
+    boolean existsDrawById(@NotNull Championship championship);
 
 }
