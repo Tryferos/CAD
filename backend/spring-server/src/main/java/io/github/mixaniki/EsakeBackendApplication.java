281f0ed629741fc5b1729c43ba2646dce47ff1ee
Vasilis Thomas <billthomas308@gmail.com>
2024-01-18 17:34:50 +0200

    Project name Refactored

--- backend/spring-server/src/main/java/io/github/mixaniki/EsakeBackendApplication.java
+++ backend/spring-server/src/main/java/io/github/mixaniki/EsakeBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BratsasApplication {
+public class EsakeBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BratsasApplication.class, args);
+		SpringApplication.run(EsakeBackendApplication.class, args);
 	}
 
 }
